825cfa04595c4ac74828700229dd7cc63957eac5
SattyS <ssathish261990@gmail.com>
2013-10-03 02:49:51 -0500

    Added Client, Send Single File done

--- proxy.cpp
+++ proxy.cpp
@@ -4,7 +4,17 @@
 #include <stdlib.h>
 #include <fstream>
 #include <functional>
+#include <stdio.h>
+#include <netinet/in.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
 #define MAX_SIZE 2
+
 
 
 #define A 54059 /* a prime */
@@ -14,7 +24,7 @@
 using namespace std;
 
 
-
+int pClientCount =0;
 //string fileNames[10] = {"One","Two","Three","Four","Five","Six","Seven","Eight","Nine","Ten"};
 
 struct webPage {
@@ -27,8 +37,10 @@ struct webPage {
 };
 
 typedef struct webPage wP;
+typedef struct sockaddr SA; 
 
 std::list<wP> wP_List;
+
 
 void cacheClear(string hash){
     //std::string f = fileNames[ind]; 
@@ -127,6 +139,7 @@ int fileWrite(string hash,string fileName){
                fprintf(ostream, buffer);
             }
        }
+       fprintf(ostream,"\n END");
        pclose(stream);
        fclose(ostream);
     }
@@ -201,40 +214,26 @@ int lRU(string text){
     return ret;
 }
 
+void sendFile(string file, int connfd){
+
+    FILE *ostream = fopen(file.c_str(),"r");
+    char buf[512];
+    char ack[2];
+    int n;
+
+    while(!feof(ostream)){
+        fgets(buf,512,ostream);
+        write(connfd,(void *) &buf,512);
+        bzero(buf,512);
+    }
+    cout << "File Sent to Client" << endl;
+    fclose(ostream);
+}
+
 int main(int argc, char const *argv[])
 {
-    /* code */
+    
     /*
-    if(argc != 2){
-        return 1;
-    }
-    std::string webPageName(argv[1]);
-    cout<<webPageName<<endl;
-    */
-    /*
-    lRU("www.google.com");
-    lRU("www.yahoo.com");
-    lRU("www.google.com");
-    lRU("www.utd.com");
-    lRU("www.utd.com");
-    lRU("www.yahoo.com");
-    */
-    /*
-    wP wPObj[10];
-    wPObj[0].webPageName = "www.google.com";
-    wPObj[0].webPageType = "text/html";
-    wPObj[1].webPageName = "www.google1.com";
-    wPObj[1].webPageType = "text1/html";
-    wPObj[2].webPageName = "www.google2.com";
-    wPObj[2].webPageType = "text2/html";
-
-    wP_List.push_front(wPObj[0]);
-    wP_List.push_front(wPObj[1]);
-    wP_List.push_front(wPObj[2]);
-    */
-    //printList();    
-
-    //system("GET HTTP/1.0 http://www.ece.tamu.edu/~reddy/ee602_08.html");
     int i;
     int count = 0, ret;
     string links;
@@ -245,10 +244,157 @@ int main(int argc, char const *argv[])
         cin >>  links;
         lRU(links);
     }
+    */
+    int serverSockFd, connfd, len;
+    //int ret;
+    int clientStatus = 0;
+    struct sockaddr_in servAddr,cli[10];
+    
+    fd_set master;
+    fd_set read_fds;
+    int fdmax;
+    int temp;
+    int i=0;
+    int s=0;
+    char tempc[20];
+    int readbytes;
+    char buf[500];
 
 
-    //checkHead("2723130594","www.w3.org");
+    serverSockFd = socket(AF_INET,SOCK_STREAM,0);
+    if(serverSockFd == -1)
+    {
+        printf("Server Socket creation failed . \n");
+        //ret = -1;
+    }
+    else{
+        printf("Server Socket successfully created . \n");
+    }
+    
 
+    bzero(&servAddr,sizeof(servAddr));
+    bzero(buf,500);
+
+    servAddr.sin_family = AF_INET;
+    servAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    servAddr.sin_port = htons(atoi(argv[1]));
+
+    if((bind(serverSockFd, (SA*)&servAddr, sizeof(servAddr)))!=0)
+    {
+        printf("Server Socket bind failed . \n");
+        exit(1);
+        //ret = -1;
+    }
+    else
+    {
+        printf("Server Socket successfully bound . \n");
+    }
+    
+    if((listen(serverSockFd, 10))!=0)
+    {
+        printf("Server Listen failed...\n");
+        exit(1);
+        //ret = -1;
+    }
+    else
+    {
+        printf("Server listening . \n");
+    }
+
+    FD_SET(serverSockFd, &master);
+    fdmax = serverSockFd;
+
+    for(;;){
+        read_fds = master;
+        if(select(fdmax+1, &read_fds, NULL, NULL, NULL) == -1){
+            perror("select");
+            exit(4);
+        }
+
+        for(i=0 ; i<=fdmax ; i++){
+            if(FD_ISSET(i, &read_fds)){
+                if(i == serverSockFd){
+                    //new Connections
+                    cout << "New Connection" << endl;
+                    len = sizeof(cli[pClientCount]);
+                    connfd = accept(serverSockFd,(SA *)&cli[pClientCount],(socklen_t * __restrict__)&len);
+                    if(connfd < 0)
+                    {
+                        printf("Server acccept failed . \n");
+                        //ret = -1;
+                    }
+                    else
+                    {
+                        cout << "Server accepted client" << endl;
+                        FD_SET(connfd, &master);
+                        if(connfd > fdmax){
+                            fdmax = connfd;
+                        }
+                        if ((readbytes = read(connfd,buf,sizeof buf)) > 0) {
+                            
+                            cout << "Inside read " << readbytes << endl;
+                            //buf[readbytes] = '\0';
+                            cout << buf << endl;
+                            //string tempBuf = buf;
+                            //string webPage = tempBuf.substr(11);
+                            //cout << webPage << endl;
+                            lRU(buf);
+                            string fileHash = hash_str(buf);
+                            sendFile(fileHash,connfd);
+                            pClientCount++;
+                            
+                        }
+                          
+                    }
+                }/*else{
+                    // handle data from a client
+                    if ((readbytes = read(i,(SM *) &clientMessage,sizeof(clientMessage))) <= 0) {
+                        // got error or connection closed by client
+                        if (readbytes == 0) {
+                            // connection closed
+                            printf("selectserver: socket %d hung up\n", i);
+                        } else {
+                            perror("recv");
+                        }
+                        clientMessage = constructServerPacket(i,clientMessage,6);
+                        clearClient(i);
+                        close(i);
+                        FD_CLR(i, &master); // remove from master set
+                        broadcast(fdmax,master,serverSockFd,i,clientMessage);
+                    }
+                    else{
+                        //send data to clients
+                        clientAttribute[1] = clientMessage.attribute[0];
+                        clientHeader.vrsn = 3;
+                        clientHeader.type = '3';
+                        clientHeader.length = 2;
+                        clientAttribute[0].type = 2;
+                        clientAttribute[1].type = 4;
+
+                        for(s=0; s<pclientCount; s++){
+                            
+                            if(clients[s].active == 1){
+
+                                if(clients[s].fd == i){
+
+                                    strcpy(tempc, clients[s].username);
+                                    strcpy(clientAttribute[0].payload, tempc);
+                                    clientAttribute[0].length = strlen(clientAttribute[0].payload);
+                                    clientAttribute[1].length = strlen(clientAttribute[1].payload);
+                                    clientMessage.attribute[0] = clientAttribute[0];
+                                    clientMessage.attribute[1] = clientAttribute[1];
+                                    clientMessage.header = clientHeader;
+                                }   
+                            }
+                            
+                        } 
+                        broadcast(fdmax,master,serverSockFd,i,clientMessage);   
+                    }
+                }*/
+        }
+    }
+}
+    close(serverSockFd);
     return 0;
 }
 
